5c7df30d766225006dcb8039bfcac6eabc17f429
jantacorina <cori.corinaa@yahoo.com>
2016-03-23 14:57:00 +0200

    corina

---
+++ test2/Source.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main()
+{
+	int a, b, s;
+	printf("a=");
+	scanf("%d", &a);
+	printf("b=");
+	scanf("%d", &b);
+	s = a + b;
+	printf("Suma numerelor %d si %d este %d \n",a,b,s);
+}
